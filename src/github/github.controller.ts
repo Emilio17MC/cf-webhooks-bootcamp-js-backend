5d18ca6ebcb3f91c5989ec38ffecacb5f71d60b1
Emilio <emilio17mc@gmail.com>
2023-11-30 18:15:53 -0600

    Crear el manejador de evento para peticiones POST que vendrán desde GitHub

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { GithubService } from './github.service';
 
 @Controller('github')
 export class GithubController {
   constructor(private readonly githubService: GithubService) {}
+
+  @Post('/')
+  webhookHandler(
+    @Headers('x-github-event') githubEvent: any,
+    @Body() body: any,
+  ) {
+    console.log({ githubEvent });
+    return { githubEvent };
+  }
 }
